f8edb706161678cdbd73f0061060f065f46284ae
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2026-04-21 00:24:31 +0600

    model add

--- prediction_app/admin.py
+++ prediction_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from prediction_app.models import HourlyData, PredictionFeedback, ModelPerformance
 # Register your models here.
+
+admin.site.register(HourlyData)
+admin.site.register(PredictionFeedback)
+admin.site.register(ModelPerformance)
