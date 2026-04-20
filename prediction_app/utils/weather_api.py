c8cec5dff7155a489baae30a9fd1fefccada1638
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2026-04-21 00:25:42 +0600

    add

--- prediction_app/utils/weather_api.py
+++ prediction_app/utils/weather_api.py
@@ -1,23 +1,17 @@
 import requests
 from datetime import datetime, timedelta
-from django.conf import settings
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class WeatherAPI:
     def __init__(self, api_key=None):
-        self.api_key = api_key or os.getenv('OPENWEATHER_API_KEY', 'YOUR_API_KEY')
-        self.base_url = "http://api.openweathermap.org/data/2.5"
+        # Use your actual API key
+        self.api_key = api_key or 'ffc34d91a216267b5a671fbe5d32c3e29'  # Your key
+        self.use_mock = False  # Set to False to use real API
         
     def get_forecast_for_hour(self, target_datetime, lat=23.8103, lon=90.4125):
-        """
-        Get weather forecast for specific hour
-        target_datetime: datetime object for target hour (e.g., 2026-04-20 19:00:00)
-        """
-        # OpenWeatherMap gives 3-hour intervals, we need to interpolate
-        url = f"{self.base_url}/forecast"
+        """Get weather forecast for specific hour"""
+        
+        url = f"http://api.openweathermap.org/data/2.5/forecast"
         params = {
             'lat': lat,
             'lon': lon,
@@ -42,7 +36,7 @@ class WeatherAPI:
                     min_time_diff = time_diff
                     closest_forecast = forecast
             
-            if closest_forecast and min_time_diff <= 10800:  # Within 3 hours
+            if closest_forecast and min_time_diff <= 10800:
                 weather_data = {
                     'temperature': closest_forecast['main']['temp'],
                     'rainfall': closest_forecast.get('rain', {}).get('3h', 0),
@@ -55,19 +49,22 @@ class WeatherAPI:
         except requests.exceptions.RequestException as e:
             print(f"Weather API error: {e}")
         
-        # Return default values if API fails
+        # Return mock data if API fails
+        return self.get_mock_weather(target_datetime)
+    
+    def get_mock_weather(self, target_datetime):
+        """Generate realistic mock weather data"""
+        hour = target_datetime.hour
+        
+        if 6 <= hour <= 17:
+            base_temp = 28
+        else:
+            base_temp = 22
+            
         return {
-            'temperature': 25.0,
+            'temperature': base_temp,
             'rainfall': 0,
-            'wind_speed': 5.0,
+            'wind_speed': 5,
             'humidity': 70,
             'pressure': 1013
         }
-    
-    def get_24h_forecast(self, start_datetime, lat=23.8103, lon=90.4125):
-        """Get 24 hours forecast starting from start_datetime"""
-        forecasts = {}
-        for hour in range(24):
-            target_time = start_datetime + timedelta(hours=hour)
-            forecasts[target_time] = self.get_forecast_for_hour(target_time, lat, lon)
-        return forecasts
