dd955fc7ca8b4e6322d057991edc3e4ac2cf17ea
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2026-06-21 00:32:46 +0600

    remove unnecessary blank lines in urls.py for improved readability

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 from prediction_app import views
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('prediction_app.urls')),
